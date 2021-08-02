6825cae6f1d5c3f806d6b8122c0487dc672c5a8f
Carlos <danahpa@gmail.com>
2021-08-02 15:35:41 -0500

    agregamos las formulas para calcular area y perimetro de circulo triangulo y cuadrado

--- figuras.js
+++ figuras.js
@@ -1,1 +1,51 @@
-console.log("hello word")
+//Codigo del cuadrado
+console.group("Cuadrados");
+const ladoCuadrado= 5;
+console.log("los lados del cuadrado miden: " + ladoCuadrado +(" cm"));
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("el perimetro del cuadrado es: " + perimetroCuadrado+(" cm"));
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("el area del cuadrado es: " + areaCuadrado +(" cm 2"));
+console.groupEnd();
+
+//Codigo del triangulo
+console.group("Triangulos");
+const ladoTriangulo1= 6;
+const ladoTriangulo2=6;
+const baseTriangulo =4;
+const alturaTriangulo=5.5;
+
+console.log(
+    "los lados del triangulo miden: " 
+    + ladoTriangulo1 +(" cm,")
+    + ladoTriangulo2 +(" cm, ")
+    + baseTriangulo +("cm")
+    );
+
+const perimetroTriangulo= ladoTriangulo1+ ladoTriangulo2 +baseTriangulo;
+
+console.log("El perimetro del triangulo es: " + perimetroTriangulo +(" cm"));
+
+const areaTriangulo=(baseTriangulo*alturaTriangulo)/2;
+
+console.log("El area del triangulo es: " + areaTriangulo +(" cm 2"));
+
+console.groupEnd();
+
+//Codigo del circulo
+console.group("circulos");
+const radioCirculo= 4;
+const diametroCirculo = radioCirculo*2;
+const pi= Math.PI;
+
+console.log("pi es: " + pi ); 
+
+const perimetroCirculo= diametroCirculo*pi;
+console.log("el perimetro del circulo es: " + perimetroCirculo +(" cm "));
+
+const areaCirculo=(radioCirculo*radioCirculo)*pi;
+console.log("el area del circulo es: " + areaCirculo +(" cm 2"));
+
+console.groupEnd();
