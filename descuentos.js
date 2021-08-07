2a33fb756a1bab3ce485984a9ebaf2383c5098fe
carlis <danahpa@gmail.com>
2021-08-06 20:50:17 -0500

    se agrego la funcion de aceptar cupones

--- descuentos.js
+++ descuentos.js
@@ -1,4 +1,31 @@
+const cupones=[
+    {codigo:"platzi",descuento:15},
+    {codigo:"cupon",descuento:50}
+];
 
+function buscarCupon(){
+    const resultP=document.getElementById("ResultCupon");
+    const miCupon=document.getElementById("InputCupon");
+    const cuponText=miCupon.value.toString();
+    
+    if(cupones.find(cupon => cupon.codigo === cuponText)){
+        const cuponValido=cupones.find(cupon => cupon.codigo === cuponText);
+        
+        const cuponValue=cuponValido.descuento;
+        
+        const inputPrice= document.getElementById("InputCuponPrice");
+        const priceValue= inputPrice.value;
+        
+        resultP.innerText=
+            "Tu cupon es valido obtuviste el " + cuponValue +
+            "% de descuento, el total es de: $"+calcularPrecioConDescuento(priceValue,cuponValue);
+
+    
+    }
+    else{ 
+        resultP.innerText="Verifica Tu Cupon";     
+    }
+}
 
 function calcularPrecioConDescuento(precio,descuento){
     const porcentajePrecioConDescuento = 100 - descuento;
