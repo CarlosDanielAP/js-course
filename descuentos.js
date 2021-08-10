a863db57c19591756050f3c9cad669f6583df26c
carlis <danahpa@gmail.com>
2021-08-10 00:04:27 -0500

    prettier

--- descuentos.js
+++ descuentos.js
@@ -1,46 +1,44 @@
-const cupones=[
-    {codigo:"platzi",descuento:15},
-    {codigo:"cupon",descuento:50}
+const cupones = [
+  { codigo: "platzi", descuento: 15 },
+  { codigo: "cupon", descuento: 50 },
 ];
 
-function buscarCupon(){
-    const resultP=document.getElementById("ResultCupon");
-    const miCupon=document.getElementById("InputCupon");
-    const cuponText=miCupon.value.toString();
-    
-    if(cupones.find(cupon => cupon.codigo === cuponText)){
-        const cuponValido=cupones.find(cupon => cupon.codigo === cuponText);
-        
-        const cuponValue=cuponValido.descuento;
-        
-        const inputPrice= document.getElementById("InputCuponPrice");
-        const priceValue= inputPrice.value;
-        
-        resultP.innerText=
-            "Tu cupon es valido obtuviste el " + cuponValue +
-            "% de descuento, el total es de: $"+calcularPrecioConDescuento(priceValue,cuponValue);
+function buscarCupon() {
+  const resultP = document.getElementById("ResultCupon");
+  const miCupon = document.getElementById("InputCupon");
+  const cuponText = miCupon.value.toString();
+  if (cupones.find((cupon) => cupon.codigo === cuponText)) {
+    const cuponValido = cupones.find((cupon) => cupon.codigo === cuponText);
 
-    
-    }
-    else{ 
-        resultP.innerText="Verifica Tu Cupon";     
-    }
+    const cuponValue = cuponValido.descuento;
+
+    const inputPrice = document.getElementById("InputCuponPrice");
+    const priceValue = inputPrice.value;
+
+    resultP.innerText =
+      "Tu cupon es valido obtuviste el " +
+      cuponValue +
+      "% de descuento, el total es de: $" +
+      calcularPrecioConDescuento(priceValue, cuponValue);
+  } else {
+    resultP.innerText = "Verifica Tu Cupon";
+  }
 }
 
-function calcularPrecioConDescuento(precio,descuento){
-    const porcentajePrecioConDescuento = 100 - descuento;
-    const PrecioConDescuento=(precio*porcentajePrecioConDescuento)/100;
-    return PrecioConDescuento;
+function calcularPrecioConDescuento(precio, descuento) {
+  const porcentajePrecioConDescuento = 100 - descuento;
+  const PrecioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+  return PrecioConDescuento;
 }
 
-function onClickButtonPriceDiscount(){
-    const inputPrice=document.getElementById("InputPrice");
-    const priceValue= inputPrice.value;
-    const inputDiscount=document.getElementById("InputDiscount");
-    const inputValue=inputDiscount.value;
+function onClickButtonPriceDiscount() {
+  const inputPrice = document.getElementById("InputPrice");
+  const priceValue = inputPrice.value;
+  const inputDiscount = document.getElementById("InputDiscount");
+  const inputValue = inputDiscount.value;
 
-    const PrecioConDescuento=calcularPrecioConDescuento(priceValue,inputValue);
+  const PrecioConDescuento = calcularPrecioConDescuento(priceValue, inputValue);
 
-    const resultP=document.getElementById("ResultP");
-    resultP.innerText="El Precio Con Descuento Es: $"+ PrecioConDescuento;
+  const resultP = document.getElementById("ResultP");
+  resultP.innerText = "El Precio Con Descuento Es: $" + PrecioConDescuento;
 }
--- figuras.js
+++ figuras.js
@@ -1,108 +1,96 @@
 //Codigo del cuadrado
 console.group("Cuadrados");
 
-
-function perimetroCuadrado(lado) { 
-    return lado * 4;
+function perimetroCuadrado(lado) {
+  return lado * 4;
 }
 
-
-function areaCuadrado(lado){  
-    return lado * lado;
+function areaCuadrado(lado) {
+  return lado * lado;
 }
 
 console.groupEnd();
 
 //Codigo del triangulo
 console.group("Triangulos");
-const ladoTriangulo1= 6;
-const ladoTriangulo2=6;
-const baseTriangulo =4;
-const alturaTriangulo=5.5;
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+const alturaTriangulo = 5.5;
 
-
-
-function perimetroTriangulo(lado1, lado2, base){
-    return lado1+lado2+base;
-
+function perimetroTriangulo(lado1, lado2, base) {
+  return lado1 + lado2 + base;
 }
 
-
-
-function areaTriangulo(base, altura){
-    return (base*altura)/2;
+function areaTriangulo(base, altura) {
+  return (base * altura) / 2;
 }
 
-function verificarIsoceles(base,lado1, lado2){
-    if(lado1==lado2){
-        if(base==lado1){
-            return false;
-        }
-        else{
-            return true;
-        }
+function verificarIsoceles(base, lado1, lado2) {
+  if (lado1 == lado2) {
+    if (base == lado1) {
+      return false;
+    } else {
+      return true;
     }
-    else{
-        return false;
-    }
+  } else {
+    return false;
+  }
 }
 
-function calcularAlturaTriangulo(base,lado1){
-    return Math.sqrt((lado1*lado1)-(base*base));
+function calcularAlturaTriangulo(base, lado1) {
+  return Math.sqrt(lado1 * lado1 - base * base);
 }
 
 console.groupEnd();
 
 //Codigo del circulo
 console.group("circulos");
-const radioCirculo= 4;
+const radioCirculo = 4;
 
-const pi= Math.PI;
+const pi = Math.PI;
 
-function diametroCirculo(radio){
-    return  radio*2;
+function diametroCirculo(radio) {
+  return radio * 2;
 }
 
-function perimetroCirculo(radio){
-    const diametro=diametroCirculo(radio);
-    return diametro*pi;
+function perimetroCirculo(radio) {
+  const diametro = diametroCirculo(radio);
+  return diametro * pi;
 }
 
-function areaCirculo(radio){
-    return (radio*radio)*pi;
+function areaCirculo(radio) {
+  return radio * radio * pi;
 }
-
 
 console.groupEnd();
 
 // interaccion con html
 
-function calcularPerimetroCuadrado(){
-    const input= document.getElementById("InputCuadrado");
-    const value = input.value;
+function calcularPerimetroCuadrado() {
+  const input = document.getElementById("InputCuadrado");
+  const value = input.value;
 
-    const perimetro= perimetroCuadrado(value);
-    alert(perimetro);
+  const perimetro = perimetroCuadrado(value);
+  alert(perimetro);
 }
 
-function calcularAreaCuadrado(){
-    const input= document.getElementById("InputCuadrado");
-    const value = input.value;
+function calcularAreaCuadrado() {
+  const input = document.getElementById("InputCuadrado");
+  const value = input.value;
 
-    const area= areaCuadrado(value);
-    alert(area);
-
+  const area = areaCuadrado(value);
+  alert(area);
 }
 
-function calcularAlturaTrianguloIsosceles(){
-   const milado1 =document.getElementById("Lado1").value;
-   const milado2 =document.getElementById("Lado2").value;
-   const mibase =document.getElementById("Base").value;
-   if(!verificarIsoceles(mibase,milado1,milado2)){
-       alert("No es un tiangulo isoceles");
-   }
-   else{
-       const altura=calcularAlturaTriangulo(mibase/2,milado1)
-        alert(altura);
-   }
+function calcularAlturaTrianguloIsosceles() {
+  const milado1 = document.getElementById("Lado1").value;
+  const milado2 = document.getElementById("Lado2").value;
+  const mibase = document.getElementById("Base").value;
+  if (!verificarIsoceles(mibase, milado1, milado2)) {
+    alert("No es un tiangulo isoceles");
+  } else {
+    const altura = calcularAlturaTriangulo(mibase / 2, milado1);
+    alert(altura);
+  }
 }
